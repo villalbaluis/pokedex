fd9ce6b1921c242dfc95f124376ccf480d6008c9
luchoFlyr <luis.villalba@flyr.com>
2026-07-22 11:38:05 -0500

    Implementation of the search for the pokemon filtered per region selected.

--- src/app/pokemon/home/home.ts
+++ src/app/pokemon/home/home.ts
@@ -1,4 +1,6 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { PokemonService } from '../pokemon.service';
 import { RegionService } from '../region.service';
 import { PokemonListItem } from '../pokemon.model';
@@ -6,20 +8,28 @@ import { PokemonCard } from './pokemon-card/pokemon-card';
 import { RegionNav } from './region-nav/region-nav';
 
 const PAGE_SIZE = 20;
+const NATIONAL_DEX_LIMIT = 2000;
 
 @Component({
   selector: 'app-home',
-  imports: [PokemonCard, RegionNav],
+  imports: [PokemonCard, RegionNav, ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
   private readonly pokemonService = inject(PokemonService);
   private readonly regionService = inject(RegionService);
-
   protected readonly pokemonList = signal<PokemonListItem[]>([]);
   protected readonly visibleCount = signal(PAGE_SIZE);
   protected readonly loading = signal(true);
+  protected readonly searchControl = new FormControl('', { nonNullable: true });
+  protected readonly searchTerm = toSignal(this.searchControl.valueChanges, { initialValue: '' });
+
+  protected readonly filteredList = computed(() => {
+    const term = this.searchTerm().trim().toLowerCase();
+    const list = this.pokemonList();
+    return term ? list.filter((item) => item.name.includes(term)) : list;
+  });
 
   constructor() {
     effect(() => {
@@ -33,11 +43,16 @@ export class Home {
           this.loading.set(false);
         });
       } else {
-        this.pokemonService.getList(20, 0).subscribe((response) => {
+        this.pokemonService.getList(NATIONAL_DEX_LIMIT, 0).subscribe((response) => {
           this.pokemonList.set(response.results);
           this.loading.set(false);
         });
       }
+    });
+
+    effect(() => {
+      this.searchTerm();
+      this.visibleCount.set(PAGE_SIZE);
     });
   }
 
