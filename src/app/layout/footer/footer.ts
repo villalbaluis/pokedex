baf128fa6ecb3b894196ca5a73c439fef5b298c5
villalbaluis <luisvillalba244@gmail.com>
2026-07-26 00:38:28 -0500

    refactor of footer, navbar and load more button

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -4,8 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrl: './footer.scss',
 })
 export class Footer {
   protected readonly year = new Date().getFullYear();
+
+  protected readonly regionCount = 10;
+
+  protected readonly links = [
+    { label: 'Pokémon', href: 'https://www.pokemon.com/' },
+    { label: 'PokeAPI', href: 'https://pokeapi.co/' },
+    { label: 'Documentación', href: 'https://pokeapi.co/docs/v2' },
+  ];
+
+  protected readonly socials = [
+    { label: 'GitHub', href: 'https://github.com/villalbaluis' },
+    { label: 'LinkedIn', href: 'https://www.linkedin.com/in/villalbaluiz/' },
+  ];
 }
